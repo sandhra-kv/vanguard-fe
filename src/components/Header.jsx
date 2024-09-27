69312a7221a5acf0cd877520aff4d9be6f8961ab
Sandhra <sandhra.mary@keyvalue.systems>
2024-09-27 11:52:02 +0530

    Fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="bg-white h-16 shadow flex items-center justify-end px-4 pr-11 gap-3">
+    <header className="bg-white h-16 shadow flex items-center justify-end px-4 pr-11 gap-3 w-full">
       <button className="h-10 w-10 shrink-0 rounded-full bg-[#F0F2F5] flex items-center justify-center">
         <img src="/icons/bell.svg" alt="bell"/>
       </button>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,9 +8,11 @@ const Layout = () => {
       <div className="w-64">
         <Sidebar />
       </div>
-      <div className="flex-1 flex flex-col">
-        <Header />
-        <main className="p-4 flex-1 bg-[#F9FAFB]">
+      <div className="flex-1 flex flex-col h-screen overflow-hidden">
+        <div className="h-16">
+          <Header />
+        </div>
+        <main className="p-4 flex-1 bg-[#F9FAFB] h-full overflow-y-auto">
           <Outlet />
         </main>
       </div>
