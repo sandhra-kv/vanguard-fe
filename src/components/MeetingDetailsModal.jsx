29ba745243d021f54026a44e08925fb974114385
cizil-keyvalue <cizil@keyvalue.systems>
2024-09-27 13:46:50 +0530

    feat: Add tabs for meeting modal

--- src/components/MeetingDetailsModal.jsx
+++ src/components/MeetingDetailsModal.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 import Modal from "../components/Modal";
-import { formatISODate } from "../utils/common";
 import VideoPlayer from "./VideoPlayer";
+import Tab from "./Tab";
+import { formatISODate } from "../utils/common";
 
 const MeetingDetails = ({
   showModal,
@@ -10,6 +13,14 @@ const MeetingDetails = ({
   duration,
   videoSrc,
 }) => {
+  const content = "<p>Hello <strong>World</strong>!</p>";
+
+  const [selectedTab, setSelectedTab] = useState("Summary");
+
+  const handleTab = (tab) => {
+    setSelectedTab(tab);
+  };
+
   return (
     <div>
       <Modal isOpen={showModal} onClose={closeModal}>
@@ -26,8 +37,19 @@ const MeetingDetails = ({
         <div className="py-6 px-9 flex flex-row gap-7">
           <div className="flex flex-col gap-4">
             <VideoPlayer src={videoSrc} poster="" width="640" height="360" />
-            {/* TODO: Replace with the tabs component */}
-            <div className="h-[312px]"></div>
+            <div className="h-[312px]">
+              <Tab
+                tabs={["Summary", "Transcript", "Analytics"]}
+                selectedTab={selectedTab}
+                onSelect={handleTab}
+              />
+              {selectedTab === "Summary" && (
+                <div
+                  dangerouslySetInnerHTML={{ __html: content }}
+                  className="p-6"
+                />
+              )}
+            </div>
           </div>
           {/* TODO: Replace with the chat component */}
           <div className="w-[545px]"></div>
