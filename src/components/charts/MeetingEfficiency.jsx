918df0cd31d7d1342f753d36ac43ae3af644e8d3
cizil-keyvalue <cizil@keyvalue.systems>
2024-09-27 21:18:48 +0530

    feat: Add charts

---
+++ src/components/charts/MeetingEfficiency.jsx
@@ -0,0 +1,60 @@
+import React, { useRef } from 'react';
+import { Bar } from 'react-chartjs-2';
+
+function MeetingVolumeAndTimeEfficiencyChart() {
+  const chartRef = useRef(null);
+
+  const data = {
+    labels: ['Muhammadali', 'Sienna', 'Andrew'],
+    datasets: [
+      {
+        backgroundColor: "#505BC5",
+        label: 'New',
+        data: [5, 4, 6],
+        borderWidth: 1,
+      },
+      {
+        label: 'Follow-up',
+        data: [5, 6, 5],
+        backgroundColor: "#848FF5",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+          usePointStyle: true,
+          boxWidth: 8,
+          boxHeight: 8
+        }
+      }
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+        max: 12,
+        stacked: true,
+        grid: {
+          display: false
+        }
+      },
+      x: {
+        stacked: true,
+        grid: {
+          display: false
+        }
+      }
+    },
+  };
+
+  return (
+      <Bar ref={chartRef} data={data} options={options} />
+  );
+}
+
+export default MeetingVolumeAndTimeEfficiencyChart;
