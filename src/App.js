c8b25bee667b0564adaa8ea2f7200277d8b65d82
Sandhra <sandhra.mary@keyvalue.systems>
2024-09-27 22:20:48 +0530

    Remove appointemnets page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
-import Appointments from './pages/Appointments';
 import ProfilePage from './pages/ProflePage';
 import TeamPerformance from './pages/TeamPerformance';
 
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
-          <Route path="appointments" element={<Appointments />} />
           <Route path="profile" element={<ProfilePage />} />
           <Route path="team-performance" element={<TeamPerformance/>} />
         </Route>
