c8f370115f9be9fa9cf3d50abb3ea57074fd4e98
cizil-keyvalue <cizil@keyvalue.systems>
2024-09-27 15:34:02 +0530

    feat: Add profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Profile = ({ name, email, phone, url, date }) => {
+  return (
+    <div className="bg-white border border-gray-200 rounded-xl h-[216px] shadow-lg flex flex-row max-w-[780px]">
+      <div className="border-r-[1px] p-8 border-gray-200">
+        <div className="flex justify-center mt-3">
+          <div className="relative">
+            <img
+              src="https://via.placeholder.com/100"
+              alt="profile"
+              className="w-20 h-20 rounded-full object-cover"
+            />
+            <div className="absolute bottom-0 right-0">
+              <img
+                src="/icons/verified-tick.svg"
+                alt="profile"
+                className="rounded-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center flex-col w-full">
+          <div className="text-[16px] font-bold flex justify-center text-[#101928] mt-2 mb-1">
+            {name}
+          </div>
+          <p className="text-gray-500 flex  text-sm justify-center">{email}</p>
+        </div>
+      </div>
+      <div className="flex flex-wrap p-12">
+        <div className="flex flex-row gap-6 items-center w-[240px]">
+          <img src="/icons/email.svg" alt="profile" className="w-5 h-5" />
+          <div className="flex flex-col">
+            <p className="text-xs text-[#667185]">Emails</p>
+            <span className="text-[#101928] text-sm font-medium">{email}</span>
+          </div>
+        </div>
+        <div className="flex flex-row gap-6 items-center w-[240px]">
+          <img src="/icons/url.svg" alt="profile" className="w-5 h-5" />
+          <div className="flex flex-col">
+            <p className="text-xs text-[#667185]">URL</p>
+            <span className="text-[#505BC5] text-sm font-medium">{url}</span>
+          </div>
+        </div>
+        <div className="flex flex-row gap-6 items-center w-[240px]">
+          <img src="/icons/phone.svg" alt="profile" className="w-5 h-5" />
+          <div className="flex flex-col">
+            <p className="text-xs text-[#667185]">Phone Number</p>
+            <span className="text-[#101928] text-sm font-medium">{phone}</span>
+          </div>
+        </div>
+
+        <div className="flex flex-row gap-6 items-center w-[240px]">
+          <img src="/icons/phone.svg" alt="profile" className="w-5 h-5" />
+          <div className="flex flex-col">
+            <p className="text-xs text-[#667185]">Date created</p>
+            <span className="text-[#101928] text-sm font-medium">{date}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,11 +1,17 @@
+import Profile from "../components/Profile";
+
 const Appointments = () => {
-    return (
-      <div>
-        <h1 className="text-2xl font-bold">Appointments</h1>
-        <p>Welcome to your appointments!</p>
-      </div>
-    );
-  };
-  
-  export default Appointments;
-  
+  return (
+    <div>
+      {/* <Profile
+        name="Cizil Maria"
+        email="cizil@keycode.com"
+        url="cizil@keycode.com"
+        phone="08168141116"
+        date="Friday, Sept 25, 2025"
+      /> */}
+    </div>
+  );
+};
+
+export default Appointments;
