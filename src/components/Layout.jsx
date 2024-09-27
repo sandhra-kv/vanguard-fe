6c8040d5d8f1c7bad0f27b0c13c1472d5222a438
Sandhra <sandhra.mary@keyvalue.systems>
2024-09-27 11:45:53 +0530

    Dashboard sidebar fixed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,9 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="flex">
-      <Sidebar />
+      <div className="w-64">
+        <Sidebar />
+      </div>
       <div className="flex-1 flex flex-col">
         <Header />
         <main className="p-4 flex-1 bg-[#F9FAFB]">
